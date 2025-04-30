76760105ffd24599565dd24fb65b30bac2d0ac33
Jens Ouwens <99173882+The-Nibblers@users.noreply.github.com>
2025-04-30 16:20:33 +0200

    Added: some of the logic in the physicsbody

--- Cpp-Eindopdracht/Cpp-Eindopdracht/PhysicsBody.cpp
+++ Cpp-Eindopdracht/Cpp-Eindopdracht/PhysicsBody.cpp
@@ -1,4 +1,5 @@
 #include "PhysicsBody.h"
+#include <iostream>
 
 void PhysicsBody::HandlePhysics()
 {
@@ -6,17 +7,44 @@ void PhysicsBody::HandlePhysics()
 
 void PhysicsBody::ApplyGravity()
 {
-
+	int gravityForce = mass * gravityModifier;
+	ApplyForce(transform, gravityForce, Down);
 }
 
 void PhysicsBody::ApplyFriction()
 {
 }
 
-void PhysicsBody::ApplyAccelleration()
+void PhysicsBody::ApplyForce(Vector2 target, int Force, Direction direction)
 {
+	switch (direction)
+	{
+	case Up:
+		Vector2 appliedForce = Vector2(target.GetX(), target.GetY() + Force);
+		break;
+	case Down:
+		Vector2 appliedForce = Vector2(target.GetX(), target.GetY() - Force);
+		break;
+	case Left:
+		Vector2 appliedForce = Vector2(target.GetX() - Force, target.GetY());
+		break;
+	case Right:
+		Vector2 appliedForce = Vector2(target.GetX() + Force, target.GetY());
+		break;
+	default:
+		std::cout << "unkown direction";
+		break;
+	}
+
+}
+
+void PhysicsBody::ApplyAccelleration(Vector2 target, Vector2 appliedForce)
+{
+	Vector2 Accelleration = Vector2(appliedForce.GetX() / mass, appliedForce.GetY() / mass);
+
 }
 
 void PhysicsBody::ApplyVelocity()
 {
+
 }
--- Cpp-Eindopdracht/Cpp-Eindopdracht/PhysicsBody.h
+++ Cpp-Eindopdracht/Cpp-Eindopdracht/PhysicsBody.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "Vector2.h"
 
+enum Direction {
+	Up,
+	Down,
+	Left,
+	Right
+
+};
+
 class PhysicsBody
 {
 public:
@@ -8,14 +16,17 @@ public:
 	Vector2 velocity;
 	Vector2 acelleration;
 
-	int gravityForce;
+	int gravityModifier;
+	int mass;
+	int accellerationTime;
 
 	void HandlePhysics();
 
 private:
 	void ApplyGravity();
 	void ApplyFriction();
-	void ApplyAccelleration();
+	void ApplyForce(Vector2 target, int Force, Direction direction);
+	void ApplyAccelleration(Vector2 target, Vector2 appliedForce);
 	void ApplyVelocity();
 };
 
